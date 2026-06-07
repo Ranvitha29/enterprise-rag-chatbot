f19985421053a7f02296644033f72392f848fd20
Ranvitha29 <ranvithanayakwadi2023@gmail.com>
2026-06-07 16:57:59 +0530

    Remove root test_models

--- test_models.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AQ.Ab8RN6KczDNCtfmCIUsGeFTYS2uwj2jwZ8BUH8FQiuUTtu-h1Q")
-
-models = client.models.list()
-
-for m in models:
-    print(m.name)
