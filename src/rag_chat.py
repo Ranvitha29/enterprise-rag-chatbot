f647f8bfdffed1ef73a36c09a8bb8077f2e92a1b
Ranvitha29 <ranvithanayakwadi2023@gmail.com>
2026-06-07 23:54:38 +0530

    Fix Groq API configuration

--- src/rag_chat.py
+++ src/rag_chat.py
@@ -2,6 +2,7 @@ print("RAG FILE WORKING")
 
 from sentence_transformers import SentenceTransformer
 import chromadb
+import os
 from groq import Groq
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY")
@@ -87,14 +88,14 @@ Question:
 {query}
 """
 
-   response = client.chat.completions.create(
-    model="llama3-8b-8192",
-    messages=[
-        {"role": "user", "content": prompt}
-    ]
-)
+       response = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
 
-return response.choices[0].message.content
+    return response.choices[0].message.content
 # =====================
 # TERMINAL CHAT
 # =====================
